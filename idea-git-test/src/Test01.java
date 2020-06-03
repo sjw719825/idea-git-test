2ddfa190305a4da05b33ab6fcdfdc0b7ae200609
sjw719825 <sjw719825@gmail.com>
2020-06-03 16:54:46 +0800

    项目初始化

---
+++ idea-git-test/src/Test01.java
@@ -0,0 +1,12 @@
+/**
+ * @author tom
+ * @version V1.0
+ * @Package PACKAGE_NAME
+ * @date 2020/6/3 14:45
+ * @Copyright © 株式会社多言语系统研究所
+ */
+public class Test01 {
+    public static void main(String[] args) {
+        System.out.println("测试......");
+    }
+}
